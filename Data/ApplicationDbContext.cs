87ddd49eee4f9e25e733755744bf97a76e394627
Osman Sheikh <146823865+OsmanSheikh03@users.noreply.github.com>
2025-05-23 15:21:13 +0100

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using File_Mgr_EF_JWT3.Models;
+using System.IO;
+
+namespace File_Mgr_EF_JWT3.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<FileModelcs> Files { get; set; } // Register the file model
+    }
+}
+
+
---
+++ FileModelcs.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace File_Mgr_EF_JWT3.Models
+{
+    public class FileModelcs
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public long Size { get; set; } // Size in KB
+
+        public DateTime UploadDate { get; set; }
+
+        [Required]
+        public string Path { get; set; } // Physical file path
+
+        [Required]
+        public string OwnerEmail { get; set; } // User who uploaded the file
+
+        public int DownloadCount { get; set; } = 0;
+        public DateTime? LastAccessTime { get; set; }
+        
+        public string? SharedLink { get; set; } // Link for sharing the file
+        public string? LinkPassword { get; set; } // Stores hashed password for security
+
+    }
+}
